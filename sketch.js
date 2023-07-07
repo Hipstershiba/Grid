d21afa8852481325325f99a5f7857adbd51f93b9
Hipstershiba <vitorrizzato@gmail.com>
2023-07-07 12:22:18 -0300

    codigo refatorado

--- sketch.js
+++ sketch.js
@@ -1,13 +1,10 @@
-let raio; // Raio dos círculos
-let theta; // Ângulo de rotação dos círculos
 let numero_de_colunas; // Número de quadrados por linha
-let numero_de_linhas;
-let numero_total_de_quadrados;
-let lado_do_quadrado; // Tamanho do lado dos quadrados **mudar para lado_do_quadrado
+let numero_de_linhas; // Número de linhas
+let lado_do_quadrado; // Tamanho do lado dos quadrados
+let raio; // Raio dos círculos circunscritos nos quadrados
+let velocidade_da_animacao; // Velocidade da animação
+let contador_de_quadros_da_animacao; // Conta o numero de quadros da animação
 let contador_de_iteracoes; // Contador de iterações
-
-let delta_velocidade; // Variável para controlar a rotação dos círculos
-let numero_de_frames = 0; // Contador de frames
 
 function setup() {
   const canvasSize = min(windowWidth, windowHeight); // Define o tamanho do canvas baseado na menor dimensão da janela
@@ -16,92 +13,98 @@ function setup() {
   smooth()
   noStroke();
   
-  theta = 0.1;
   numero_de_colunas = 10; // Número de quadrados por linha
   numero_de_linhas = numero_de_colunas; // Número de linhas
-  numero_total_de_quadrados = numero_de_colunas * numero_de_linhas; // Número total de quadrados
   lado_do_quadrado = canvasSize / numero_de_colunas; // Tamanho do lado_do_quadrado em relação à dimensão do canvas
-  raio = sqrt(pow(lado_do_quadrado, 2) / 2); // diagonal do quadrado dividido por 2
-  contador_de_iteracoes = 0;
+  raio = sqrt(pow(lado_do_quadrado, 2) / 2); // Hipotenusa do triângulo retângulo formado pelo lado_do_quadrado
+  velocidade_da_animacao = 2; // Velocidade da animação
+  contador_de_quadros_da_animacao = 0; // conta o numero de quadros da animação
+  contador_de_iteracoes = 0; // Contador de iterações
 }
 
 function draw() {
+  // Define a cor do fundo conforme o contador de iterações
   background(atualiza_cor_do_fundo(contador_de_iteracoes));
 
+  // Preenche os quadrados com a cor do quadrado conforme o contador de iterações
   fill(atualiza_cor_do_quadrado(contador_de_iteracoes));
 
+  // Posiciona os quadrados no canvas
   desenha_xadrez();
-
-  theta = delta_velocidade;
-  delta_velocidade = 90 * pow(sin(radians(numero_de_frames / 2)), 2);
   
-  numero_de_frames += 2;
-  if (numero_de_frames > 180) {
-    numero_de_frames = 0;
+  // Atualiza o contador de quadros da animação conforme a velocidade da animação passada como parâmetro
+  atualiza_contador_de_quadros_da_animacao(velocidade_da_animacao);
+  
+  // Incrementa o contador de iterações quando a variação angular for múltipla de 90
+  if (variacao_angular() % 90 == 0 && variacao_angular() != 0) {
+    contador_de_iteracoes++;
   }
   
-  if (theta >= 90) {
-    fim_da_animacao();
-  }
+  // informa no console o número de iterações, a variação angular e o contador de quadros da animação 
+  console.log(contador_de_iteracoes, "//", variacao_angular(), "//", contador_de_quadros_da_animacao);
   
-  console.log(contador_de_iteracoes, "//", delta_velocidade, "//", numero_de_frames, "//", theta);
-  
+  // Para a animação quando o contador de iterações for igual a 2
   if (contador_de_iteracoes == 2) {
-    // noLoop();
+    noLoop();
   }
 }
 
-function desenha_quadrado(_x, _y, _deslocamento) {
-  let angulo = 45;
-  beginShape();    
-  for (let vertice = 0; vertice < 4; vertice++) {
-    vertex(_x + _deslocamento + raio * cos(radians(angulo + theta)), _y + raio * sin(radians(angulo + theta)));
-    angulo += 90;
-  }
-  endShape(CLOSE);
+// Calcula a correção de posicionamento para centralizar o xadrez no canvas
+function correcao_de_posicionamento() {
+  return (lado_do_quadrado / 2) - (width / 2);
 }
 
+// Desenha os quadrados no canvas conforme o número de colunas e linhas
 function desenha_xadrez() {
-  for (let coluna = 0; coluna < numero_de_linhas / 2; coluna++) {
+  for (let coluna = 0; coluna < numero_de_colunas / 2; coluna++) {
     for (let linha = 0; linha < numero_de_linhas; linha++) {
       desenha_quadrado(coordenada_x_do_quadrado(coluna), coordenada_y_do_quadrado(linha), deslocamento_de_x(linha, paridade_da_iteracao(contador_de_iteracoes)))
     }
   }
 }
 
-function coordenada_x_do_quadrado (_coluna) {
-  return (_coluna * (lado_do_quadrado * 2)) + correcao_de_posicionamento();
+// Calcula a coordenada x do quadrado baseado na coluna
+function coordenada_x_do_quadrado (coluna) {
+  return (coluna * (lado_do_quadrado * 2)) + correcao_de_posicionamento();
 }
 
-function coordenada_y_do_quadrado(_linha) {
-  return (_linha * lado_do_quadrado) +  correcao_de_posicionamento();
+// Calcula a coordenada y do quadrado baseado na linha
+function coordenada_y_do_quadrado(linha) {
+  return (linha * lado_do_quadrado) +  correcao_de_posicionamento();
 }
 
-function correcao_de_posicionamento() {
-  return (lado_do_quadrado / 2) - (width / 2);
-}
-
-function deslocamento_de_x(_linha, _paridade_da_iteracao) {
-  if ((_linha % 2 == 0 && _paridade_da_iteracao == "par") || (_linha % 2 != 0 && _paridade_da_iteracao == "impar")) { //se a linha for par
+// Desloca o quadrado para a direita ou para a esquerda conforme a paridade da linha e da iteração
+function deslocamento_de_x(linha, _paridade_da_iteracao) {
+  // Se a linha for par e a iteração for par ou se a linha for ímpar e a iteração for ímpar então o deslocamento é 0
+  if ((linha % 2 == 0 && _paridade_da_iteracao == "par") || (linha % 2 != 0 && _paridade_da_iteracao == "impar")) {
     return 0;
   } else {
     return lado_do_quadrado;
   }
 }
 
-function atualiza_theta() {
-  theta = delta_velocidade;
+// Desenha um quadrado com os vértices deslocados conforme a variação angular
+function desenha_quadrado(_x, _y, _deslocamento) {
+  let angulo = 45;
+  beginShape();    
+  for (let vertice = 0; vertice < 4; vertice++) {
+    vertex(_x + _deslocamento + raio * cos(radians(angulo + variacao_angular())), _y + raio * sin(radians(angulo + variacao_angular())));
+    angulo += 90;
+  }
+  endShape(CLOSE);
 }
 
-function atualiza_delta_velocidade() {
-  delta_velocidade = 90 * pow(sin(radians(numero_de_frames / 2)), 2);
+// Calcula a variação angular conforme o contador de quadros da animação
+function variacao_angular() {
+  return 90 * pow(sin(radians(contador_de_quadros_da_animacao / 2)), 2);
 }
 
-
-function atualiza_numero_de_frames() {
-  numero_de_frames += 2;
-  if (numero_de_frames > 180) {
-    numero_de_frames = 0;
+// Atualiza o contador de quadros da animação
+function atualiza_contador_de_quadros_da_animacao(velocidade_da_animacao) {
+  contador_de_quadros_da_animacao += velocidade_da_animacao;
+  // Reinicia a animação quando o contador de quadros da animação atinge 180
+  if (contador_de_quadros_da_animacao > 180) {
+    contador_de_quadros_da_animacao = 0;
   }
 }
 
@@ -131,10 +134,3 @@ function atualiza_cor_do_quadrado(iteracao) {
     return 255;
   }
 }
-
-// Reinicia os valores de theta e delta_velocidade quando a animação termina e incrementa o contador de iterações
-function fim_da_animacao() {
-  theta = 0;
-  delta_velocidade = 0;
-  contador_de_iteracoes++;
- }
